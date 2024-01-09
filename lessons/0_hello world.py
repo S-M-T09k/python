bbded11daa964bc6beddd205b3c15ce71b1fd1bb
Shwe Myo Thway <S.M.T.09k@gmail.com>
2024-01-09 16:10:57 +0630

    started python 1-variables

---
+++ lessons/0_hello world.py
@@ -0,0 +1,2 @@
+print("Hello World!")
+print("this is not going to be hard\nI hope")
---
+++ lessons/1_variables.py
@@ -0,0 +1,23 @@
+name = "Shwe Myo Thway"
+age = 16
+isDepressed = True
+
+if isDepressed == True:
+  depressedOrNot = "why are you depressed?"
+else:
+  depressedOrNot = "good for you"
+
+print("your name is "+name+"\nyou are "+str(age)+"years old\n"+depressedOrNot)
+print("your name is", name, "\nyou are", str(age), "years old\n", depressedOrNot)
+
+print(f"your name is {name}\nyou are {age} years old\n{depressedOrNot}")
+
+#set multiple at once
+
+x, y, z = 1, 2, 3
+
+#set multiple to one
+
+a = b = c = d = e = 2
+
+#?what
